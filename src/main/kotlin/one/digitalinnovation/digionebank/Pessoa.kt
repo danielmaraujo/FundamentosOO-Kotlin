a79621bdf327faf2afbe16efafa5dd67572dba96
danielmaraujo <danielaraujoas@gmail.com>
2021-08-12 17:04:28 -0300

    atualiza pessoa

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,11 +3,15 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome : String = "Daniel"
     var cpf : String = "123.123.123-12"
+    private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main(){
     val daniel = Pessoa()
 
-    println(daniel.nome)
-    println(daniel.cpf)
+    println(daniel.pessoaInfo())
 }
